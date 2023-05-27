68131f104981e205e632d638c70de7df9123c825
Polina Laptop <linv513jj@gmail.com>
2023-05-27 06:51:21 +0300

    add favicon + logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,15 +2,20 @@ import React from "react";
 
 const Header = (props) => {
   return (
-    <div className='row'>
-      <nav className="navbar navbar-dark navbar-expand-lg bg-dark"> 
-      <div >
-        <h1 style={{color:'whitesmoke', marginLeft:'30px'}}
-        >To Do</h1>
+    <div className="row">
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div style={{ display: "flex", marginLeft: "30px", alignItems: "center" }}>
+          <img
+            src="https://i.ibb.co/sR6Gbsy/Pencil.png"
+            alt="Pencil"
+            width={36}
+            height={36}
+          />
+          <h1 style={{ color: "whitesmoke", marginLeft: "16px" }}>To Do</h1>
         </div>
       </nav>
     </div>
-  )
+  );
 };
 
 export default Header;
